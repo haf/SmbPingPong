6dc4d5dee72686ca9056f720882b7b7e485e219e
Henrik Feldt <henrik@haf.se>
2016-02-16 13:35:07 +0100

    initial commit

---
+++ Rakefile
@@ -0,0 +1,46 @@
+require 'bundler/setup'
+
+require 'albacore'
+# require 'albacore/tasks/release'
+require 'albacore/tasks/versionizer'
+require 'albacore/ext/teamcity'
+
+Configuration = ENV['CONFIGURATION'] || 'Release'
+
+Albacore::Tasks::Versionizer.new :versioning
+
+desc 'create assembly infos'
+asmver_files :assembly_info do |a|
+  a.files = FileList['**/*proj'] # optional, will find all projects recursively by default
+  a.attributes assembly_description: 'TODO',
+               assembly_configuration: Configuration,
+               assembly_company: 'Foretag AB',
+               assembly_copyright: "(c) 2016 by John Doe",
+               assembly_version: ENV['LONG_VERSION'],
+               assembly_file_version: ENV['LONG_VERSION'],
+               assembly_informational_version: ENV['BUILD_VERSION']
+end
+
+desc 'Perform fast build (warn: doesn\'t d/l deps)'
+build :quick_compile do |b|
+  b.prop 'Configuration', Configuration
+  b.logging = 'detailed'
+  b.sln     = 'SmbPingPong/SmbPingPong.sln'
+end
+
+task :paket_bootstrap do
+system 'tools/paket.bootstrapper.exe', clr_command: true unless   File.exists? 'tools/paket.exe'
+end
+
+desc 'restore all nugets as per the packages.config files'
+task :restore => :paket_bootstrap do
+  system 'tools/paket.exe', 'restore', clr_command: true
+end
+
+desc 'Perform full build'
+build :compile => [:versioning, :restore, :assembly_info] do |b|
+  b.prop 'Configuration', Configuration
+  b.sln = 'SmbPingPong/SmbPingPong.sln'
+end
+
+task :default => :compile
---
+++ SmbPingPong/Program.fs
@@ -0,0 +1,111 @@
+﻿module Program
+
+open System
+open System.Text
+open System.IO
+open System.Globalization
+open System.Threading
+open System.Security.Cryptography
+open Argu
+open fszmq
+open fszmq.Context
+open fszmq.Socket
+  
+let inline encode value = value |> string |> Encoding.UTF8.GetBytes
+let inline decode value = Encoding.UTF8.GetString value
+let inline spawn fn = Thread(ThreadStart fn).Start()
+
+// mono  --debug ./SmbPingPong.exe --directory $(pwd) --pong-mode
+let pong directory =
+  use context = new Context()
+  use server  = rep context
+  bind server "tcp://*:5556"
+
+  let rec loop () =
+    match server |> recv |> decode with
+    | fileName when fileName.EndsWith ".txt" ->
+      let path = Path.Combine (directory, fileName)
+      let contents = File.ReadAllText path
+
+      if contents.Length = 0 then
+        eprintfn "File contents are empty at path %s" path
+        "NACK"B |>> server
+        loop ()
+      else
+        printfn "File contents length: %i" contents.Length
+        "ACK"B |>> server
+        loop ()
+
+    | _ ->
+      "goodbye"B |>> server
+
+  loop ()
+
+let createFileName () =
+  Guid.NewGuid().ToString().Replace("-", "").ToLowerInvariant() + ".txt"
+
+let rng = RandomNumberGenerator.Create()
+
+let createFile filePath =
+  let rndData = Array.zeroCreate 8
+  do rng.GetBytes rndData
+  let rndNo = BitConverter.ToInt64(rndData, 0)
+  let contents = rndNo.ToString(CultureInfo.InvariantCulture)
+  use fs = File.OpenWrite(filePath)
+  use sw = new StreamWriter(fs)
+  sw.WriteLine contents
+  contents
+
+// mono  --debug ./SmbPingPong.exe --directory $(pwd) --ping-mode tcp://127.0.0.1:5556
+let ping connectTo dir =
+  use context = new Context()
+  use client  = req context
+  connectTo |> connect client
+
+  let rec loop i =
+    let fileName = createFileName ()
+    let filePath = Path.Combine (dir, fileName)
+    let contents = createFile filePath
+    fileName |> encode |> send client
+    printfn "(%i) sent: %s with contents %s" i filePath contents
+
+    match recv client with
+    | null ->
+      ()
+
+    | bs ->
+      let reply = decode bs
+      printfn "(%i) got: %s" i reply
+      loop (i + 1u)
+
+  loop 1u
+
+type Args =
+  | [<Mandatory>] Directory of string
+  | [<PrintLabels>] Ping_Mode of other:string
+  | Pong_Mode
+with
+  interface IArgParserTemplate with
+    member s.Usage =
+      match s with
+      | Directory _ -> "Where to write files to in Ping mode"
+      | Ping_Mode _ -> "Runs Rutta in Shipper/PUB mode (send Messages from a node to proxy)"
+      | Pong_Mode -> "Server/receiver that tries to read the file from disk"
+
+[<EntryPoint>]
+let main argv =
+  let parser = ArgumentParser.Create<Args>()
+  let parsed = parser.Parse argv
+  let dir = parsed.GetResult <@ Directory @>
+  let pingMode = parsed.TryGetResult <@ Ping_Mode @>
+
+  printfn "libzmq version: %A" ZMQ.version
+
+  match pingMode with
+  | Some pongBinding ->
+    ping pongBinding dir
+
+  | _ ->
+    pong dir
+
+  0
